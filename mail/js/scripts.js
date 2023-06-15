2959ef4f29810bc58b9042b077d9c92a6fa26612
stiva98 <samustiva9810@gmail.com>
2023-06-15 18:13:25 +0200

    mail ora funzonante risolto modifica di un errore

--- mail/js/scripts.js
+++ mail/js/scripts.js
@@ -1,10 +1,11 @@
 const email = document.getElementById('email');
 const myArray = ['samuele@gmail.com', 'stivaletti@gmail.com', 'pincopallino@gmail.com', 'pallino@gmail.com'];
+//console.log(myArray)
 
 const myButton = document.querySelector('button'); 
 myButton.addEventListener('click',function(){
     const emailval = email.value;
-    const flag = 0;
+    let flag = 0;
     for (let i = 0; i<myArray.length; i++) {
         
         if(emailval == myArray[i]) {
@@ -13,9 +14,11 @@ myButton.addEventListener('click',function(){
     }
 
     if(flag == 1) {
-        console.log ('Puoi accedere')
+        //console.log('Puoi accedere')
+        alert('email valida')
     }else {
-        console.log('Non puoi accedere')
+        //console.log('Non puoi accedere')
+        alert('email non valida')
     }
     
 });
