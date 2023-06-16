0380a613e600ef11e60c4ef646290fdced5e76c7
stiva98 <samustiva9810@gmail.com>
2023-06-16 09:59:14 +0200

    cambiato variabile da let a const

--- dadi/js/scripts.js
+++ dadi/js/scripts.js
@@ -1,18 +1,18 @@
 const myButton = document.querySelector ('button');
 myButton.addEventListener('click',function(){
-    let user = document.querySelector('.user')
+    const user = document.querySelector('.user')
     //console.log(user)
-    let pc = document.querySelector('.pc')
+    const pc = document.querySelector('.pc')
     //console.log(pc)
-    let result = document.querySelector('.result')
+    const result = document.querySelector('.result')
     //console.log(result)
 
     //generazione numeri random 
-    let randomUser = Math.floor(Math.random() * 6) + 1;
+    const randomUser = Math.floor(Math.random() * 6) + 1;
     //console.log('numero random user',randomUser);
     user.innerHTML = (randomUser);
 
-    let randomPc = Math.floor(Math.random() * 6) + 1;
+    const randomPc = Math.floor(Math.random() * 6) + 1;
     //console.log('numero random pc',randomPc);
     pc.innerHTML = (randomPc);
 
